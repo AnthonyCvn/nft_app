b3f0c6e866649cadcd1e9315ca21b3c967659657
Anthony Cavin <anthony.cavin.ac@gmail.com>
2021-11-10 23:53:24 +0100

    Update Data

--- nft_app.py
+++ nft_app.py
@@ -2,42 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
-
-import streamlit.components.v1 as components  # Import Streamlit
-
-# Render the h1 block, contained in a frame of size 200x200.
-components.html(
-    """
-    <!-- Begin Mailchimp Signup Form -->
-    <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7_dtp.css" rel="stylesheet" type="text/css">
-    <style type="text/css">
-        #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-        We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-    </style>
-    <div id="mc_embed_signup">
-    <form action="https://gmail.us20.list-manage.com/subscribe/post?u=caa5f8f8430da8ccdec49101c&amp;id=4ed90e842d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-        <div id="mc_embed_signup_scroll">
-        <label for="mce-EMAIL">Subscribe</label>
-        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_caa5f8f8430da8ccdec49101c_4ed90e842d" tabindex="-1" value=""></div>
-            <div class="optionalParent">
-                <div class="clear foot">
-                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-                    <p class="brandingLogo"><a href="http://eepurl.com/hM8r_L" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"></a></p>
-                </div>
-            </div>
-        </div>
-    </form>
-    </div>
-
-    <!--End mc_embed_signup-->
-    """
-)
-
-
 # Streamlit title and text
 st.title("NFT analysis")
 
